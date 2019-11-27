d8cb8bdf8b2e351f22859b7e8f37074474e578a1
caojilin <caojilin@berkeley.edu>
2019-11-27 00:37:42 -0800

    fix

--- components/components/accounts/models.py
+++ components/components/accounts/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from datetime import datetime
+from django.conf import settings
 
 
 # Create your models here.
 class Album(models.Model):
+
     image = models.ImageField(upload_to='photo/%y/%m/%d/')
     list_date = models.DateTimeField(default=datetime.now, blank=True)
