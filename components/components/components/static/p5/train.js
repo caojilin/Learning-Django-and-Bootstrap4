80e099be488f6d8d22016743c6e83c8ddb3dfa0e
caojilin <caojilin@berkeley.edu>
2019-11-27 16:57:35 -0800

    wht

--- components/components/components/static/p5/train.js
+++ components/components/components/static/p5/train.js
@@ -1,28 +1,28 @@
-// Daniel Shiffman
-// https://thecodingtrain.com/CodingChallenges/147-chrome-dinosaur.html
-// https://youtu.be/l0HoJHc-63Q
-
-// Google Chrome Dinosaur Game (Unicorn, run!)
-// https://editor.p5js.org/codingtrain/sketches/v3thq2uhk
-
 class Train {
 
-    constructor() {
-      this.r = 75;
-      this.x = width;
-      this.y = height - this.r;
-    }
-  
-    move() {
-      this.x -= 16;
-    }
-  
-    show() {
-      image(tImg, this.x, this.y, this.r, this.r);
-  
-      // fill(255, 50);
-      // ellipseMode(CORNER);
-      // ellipse(this.x, this.y, this.r, this.r);
-    }
-  
+  constructor() {
+    this.r = 75;
+    this.x = width;
+    this.y = height - this.r;
   }
+
+  move(units) {
+    if (units == null)
+      units = 16
+
+    this.x -= units;
+  }
+
+  show() {
+    image(tImg, this.x, this.y, this.r, this.r);
+    fill(255, 50);
+    //ellipseMode(CORNER);
+    //ellipse(this.x, this.y, this.r, this.r);
+
+  }
+  
+  offscreen() {
+    return this.x < -25
+  }
+
+}
